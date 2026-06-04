b0cd0af097859dbf18187bd9b85ef810f9d436d7
a40148 <a40148@esas.pt>
2026-06-04 18:16:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  // Base path obrigatório com o nome exato do teu repositório no GitHub Pages
   base: '/green-website/', 
-  plugins: [
-    tailwindcss(),
-  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        solucoes: resolve(__dirname, 'solucoes.html')
+      }
+    }
+  }
 })
